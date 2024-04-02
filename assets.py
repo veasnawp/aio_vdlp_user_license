1d46c334e8d444887d894503c29c8f26db62d5d7
Veasna Kheng <114059731+veasnawp@users.noreply.github.com>
2024-04-03 04:07:54 +0700

    Update assets.py

--- assets.py
+++ assets.py
@@ -1,11 +1,26 @@
 import json
+from urllib.error import URLError
 
 # from typing import Literal
 from urllib.request import Request, urlopen
 
+KEY_URLOPENRET = [
+    # "fp", # <_io.BufferedReader name=400>
+    "debuglevel", # int
+    "_method", # Literal["GET", "POST", "PUT", ...]
+    "version", # int
+    "headers", # <http.client.HTTPMessage object at 0x00000201B0297910>
+    "msg", "reason", # Literal["OK", ...]
+    "status", "code", # Literal[200, 404, 500, ...]
+    "chunked", # bool
+    "chunk_left", # str(UNKNOWN)
+    "length", # content-length: int
+    "will_close", # bool
+    "url", # redirect url
+]
 
 def _execute_request(
-    url:str,
+    url,
     method=None,
     headers=None,
     data=None,
@@ -27,9 +42,49 @@ def _execute_request(
         raise ValueError("Invalid URL")
     return urlopen(request, timeout=timeout)  # nosec
 
+def get_execute_request(url:str, resp_option:dict[str,any]=None):
+    options = {
+        "url": url
+    }
+    if isinstance(resp_option, dict):
+        for key, val in resp_option.items():
+            if resp_option.get(key) is not None:
+                options[key] = val
+    response_dict = {}
+
+    # print(options)
+    headers = {}
+    r = _execute_request(**options)
+    r_dict = r.__dict__
+    for key in KEY_URLOPENRET:
+        if key == "headers":
+            r_headers = dict(r_dict["headers"])
+            for k, v in r_headers.items():
+                val = str(v).strip()
+                if not val.startswith("{") and not val.endswith("}"):
+                    headers[k] = v
+        else:
+            response_dict[key] = r_dict[key]
+
+    if response_dict.get("status") == 200:
+        response_dict["data"] = bytes.decode(r.read())
+
+    response_dict["headers"] = headers
+    return response_dict
+
+
+
+def isOnline():
+    url = 'https://www.google.com'
+    try:
+      r = _execute_request(url)
+      return True
+    except URLError as err:
+      # print(err.__dict__)
+      return False
+
 MachineID = "2D5BE66F-6DA9-434E-A0AC-518722E39A8E"
 domain = 'https://veasnapythonusers.blogspot.com'
-
 
 def mainAssets(id, type_):
 
@@ -38,6 +93,15 @@ def mainAssets(id, type_):
     data_dict:dict = json.loads(bytes.decode(r.read()))
     return json.loads(data_dict['entry']['content']['$t'])
 
+def isModerator(userId=None, id="1450582494671355516", type="pages"):
+  assets = mainAssets(id, type)
+  moderators = [user.get("userId") for user in assets["moderators"]]
+  if userId in moderators:
+    # print("Moderators: ", moderators)
+    return True
+  else:
+    # print("Moderators: ", assets["moderators"])
+    return False
 
 # if __name__ == "__main__":
 #     pageId = '1450582494671355516'
